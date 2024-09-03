03220c536c49e2226c597e17d106e8f5db5b291e
강문정 <moon@gangmunjeongs-MacBook-Air.local>
2024-09-03 16:06:23 +0900

    [SETUP] Configure Vue Router and add initial routes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <router-view></router-view>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+
+};
 </script>
 
 <style>
@@ -23,4 +21,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
