7fe1b30769d191adae381c45c0c97b175e565344
강문정 <moon@gangmunjeongs-MacBook-Air.local>
2024-09-04 20:00:19 +0900

    [Add] user detail modal for viewing enlarged user info

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,13 @@ import router from './router';
 
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+import DetailUserModal from './components/DetailUserModal.vue'
 
 import { mainMixin } from './mixins/mainMixin'
 
 const app = createApp(App);
 app.use(router);
 app.mixin(mainMixin)
+app.component('DetailUserModal', DetailUserModal);
 
 app.mount('#app');
